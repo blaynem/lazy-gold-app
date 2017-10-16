4df41e9f29dea8471ee3dbc8154b81acc50d38c5
Blayne Marjama <blayne@audioglobe.com>
2017-10-15 21:50:38 -0700

    api imported

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
+import axios from 'axios';
+import { apiConfig } from '../config';
+
 export function testThis() {
   return(dispatch) => {
-    console.log("tests")
+    console.log(apiConfig)
     dispatch({ type: "Test", payload: {item: "test1"} })
   }
 }
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-import { API_URL } from '../env.config';
+import { API_URL } from './env.config';
 
 export const apiConfig = {
   serverGet : `${API_URL}`
