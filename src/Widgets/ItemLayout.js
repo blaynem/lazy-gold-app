039b31a9eb86367bb7dbbdef0d2c8e35931a461c
Blayne Marjama <blayne@audioglobe.com>
2017-10-16 23:41:45 -0700

    added item layout for reusability of displaying selected item

--- src/Widgets/ItemLayout.js
+++ src/Widgets/ItemLayout.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
-export const ItemLayout = () => (
-  <div>Test</div>
+import { RankSelector } from '../Widgets/RankSelector';
+import { ObliterumYield } from '../Widgets/ObliterumYield';
+
+export const ItemLayout = ({ ahPrice, ahProfit, craftingCost, handleChange, name, obliterumProfit, obliterumYield, rankValue }) => (
+  <li>
+    <h3>{name}</h3>
+    <RankSelector value={rankValue} handleChange={handleChange}/>
+    <h3>AH Price: {ahPrice}</h3>
+    <h3>Crafting Cost: {craftingCost}</h3>
+    <ObliterumYield value={obliterumYield} handleChange={handleChange}/>
+    <h3>Obliterum Profit: {obliterumProfit}</h3>
+    <h3>AH Profit: {ahProfit}</h3>
+  </li>
 )
