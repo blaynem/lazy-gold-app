41b8d161c18ffb7d3ecb2e1ad8e434ed1876389a
Blayne Marjama <blayne.marjama@gmail.com>
2017-11-09 17:00:01 -0800

    moved item layout location

---
+++ src/components/ItemLayout.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { RankSelector } from '../Widgets/RankSelector';
+
+// imports the RankSelector and ObliterumYield selector/Fields.
+// Otherwise this is just being used to visualize our table rows
+export const ItemLayout = ({
+  ahPrice,
+  ahProfit,
+  availableRanks,
+  craftingCost,
+  handleChange,
+  name,
+  obliterumProfit,
+  obliterumYield,
+  rankValue }) => (
+  <li>
+    <h3>{name}</h3>
+    <RankSelector
+      availableRanks={availableRanks}
+      value={rankValue}
+      handleChange={handleChange}/>
+    <h3>AH Price: {ahPrice}</h3>
+    <h3>Crafting Cost: {craftingCost}</h3>
+    <h3>Obliterum Yield: {obliterumYield}</h3>
+    <h3>Obliterum Profit: {obliterumProfit}</h3>
+    <h3>AH Profit: {ahProfit}</h3>
+  </li>
+)
