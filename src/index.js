4311a936114ea6562722ae1a653acaa108690c7c
Blayne Marjama <blayne.marjama@gmail.com>
2018-04-06 17:16:44 -0700

    fixed building issue

--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(
-	reducers,
-	compose(
-		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
-)
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(thunk),
+));
 
 ReactDOM.render(
 	<Provider store={store}>
