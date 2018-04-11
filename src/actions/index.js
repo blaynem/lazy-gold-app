7c80e8f2ea7933d374bc02a032a32b6e562aefc6
Blayne Marjama <blayne.marjama@gmail.com>
2018-04-10 23:59:14 -0700

    moved calculating of crafting cost into initial load of application data so it doesn't constantly re-calculate

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,6 +13,34 @@ import { getRecipeItemsList } from '../utils'
 
 import { mockData } from '../mockData';
 
+// Used to caculate crafting cost of each rank of item
+const calculateCraftingCost = (itemRecipe, reagentData, itemData) => {
+  // map over each recipe rank per recipe,
+  // returns the original array objects with a new cost property
+  const newRecipeRank = itemRecipe.RecipeRank.map(rank => {
+    // to calculate, we iterate over the reagants of the materials needed:
+    // mat total price consists of the materials current marketValue and the amount needed to create the item
+    const craftCost = (rank.Reagents).reduce( (acc, matItem) => {
+      const matPrice = reagentData[matItem.Id].MarketValue
+      const total = matPrice * matItem.Amount
+      return acc + total
+    }, 0)
+
+    // we divide by the amount that is crafted per rank in order to get the price.
+    // Ex: Rank 1 only yields us 1, rank 3 yields us roughly 1.5
+    const costOfRank = craftCost / rank.Yield
+    return {
+      ...rank,
+      costOfRank
+    }
+  })
+  // returns the itemRecipe with the new updated recipe rank information
+  return {
+    ...itemRecipe,
+    RecipeRank: newRecipeRank,
+  }
+}
+
 // Takes an object of profession names, with an array ofvalues of an item Names
 // returns an object of profession names, with an array of values of item IDs
 const parseNamesIntoIds = () => {
@@ -43,13 +71,15 @@ const parseNeededItems = (itemDump) => {
     // finds the item in the mock data
     const itemData = itemDump.find( item => item.Id === ItemId )
     const reagentData = getRecipeItemsList(itemRecipe, itemDump)
+
+    const craftingCosts = calculateCraftingCost(itemRecipe, reagentData)
     // returns an object to be used
     return {
       ...acc,
       [ItemId]: {
         ...itemData,
         recipe: {
-          ...itemRecipe
+          ...craftingCosts
         },
         reagentData
       }
--- src/components/ItemWrapper.js
+++ src/components/ItemWrapper.js
@@ -16,23 +16,6 @@ class ItemWrapper extends Component {
     const { name, value } = e.target
     this.setState({ [name]: value })
   }
-  calculateCraftingCost = () => {
-    const { itemData } = this.props
-    const { rankValue } = this.state
-    // we get the materials from the recipeRank const defined on bottom of page
-    // const materials = recipeRank[rankValue]
-    const materials = itemData.recipe.RecipeRank[rankValue]
-    // to calculate, we iterate over the reagants of the materials needed:
-    // mat total price consists of the materials current marketValue and the amount needed to create the item
-    const costToCraft = (materials.Reagents).reduce( (acc, matItem) => {
-      const matPrice = itemData.reagentData[matItem.Id].MarketValue
-      const total = matPrice * matItem.Amount
-      return acc + total
-    }, 0)
-    // we divide by the amount that is crafted per rank in order to get the price.
-    // Ex: Rank 1 only yields us 1, rank 3 yields us roughly 1.5
-    return costToCraft / materials.Yield
-  }
   render() {
     const { rankValue } = this.state
     const {
@@ -40,6 +23,7 @@ class ItemWrapper extends Component {
       obliterumData
     } = this.props
 
+    const { Name } = itemData
     const { ObliterumYield, RecipeRank } = itemData.recipe
     if (itemData === undefined || itemData.recipe === undefined){
       console.error(`Couldn't find data for item: ${itemData.Name}`)
@@ -48,7 +32,7 @@ class ItemWrapper extends Component {
     // ahprice recieved from database
     const ahPrice = itemData.MarketValue
     // calculated crafting cost
-    const craftingCost = this.calculateCraftingCost()
+    const craftingCost = RecipeRank[rankValue].costOfRank
     // obliterum price on the AH
     const obliterumAHPrice = obliterumData.MarketValue
     // obliterum profit is the price, times the yield for obliterating the item, divided by 100.
@@ -64,7 +48,7 @@ class ItemWrapper extends Component {
         ahProfit={ahProfit}
         craftingCost={craftingCost}
         handleChange={this.handleChange}
-        name={itemData.Name}
+        name={Name}
         obliterumProfit={obliterumProfit}
         obliterumYield={ObliterumYield}
         rankValue={rankValue}
