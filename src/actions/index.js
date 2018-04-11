ef829680f368f19426a566a186f6f839e3f0d318
Blayne Marjama <blayne.marjama@gmail.com>
2018-04-10 23:10:22 -0700

    added comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,6 +13,8 @@ import { getRecipeItemsList } from '../utils'
 
 import { mockData } from '../mockData';
 
+// Takes an object of profession names, with an array ofvalues of an item Names
+// returns an object of profession names, with an array of values of item IDs
 const parseNamesIntoIds = () => {
   const parsedProfArrays = Object.keys(recipePages).reduce( (acc, profession) => {
     const itemIdArr = recipePages[profession].map( itemName => {
@@ -26,38 +28,41 @@ const parseNamesIntoIds = () => {
   return parsedProfArrays
 }
 
+// Takes an array of items objects, returns a key value of
+// ItemsID: ItemsObj with ammended properties
+// Ammended props: recipe Object, reagent Object.
+// TODO: Reagent object should be removed, as we should already have this information in the data dump.
+const parseNeededItems = (itemDump) => {
+  return Object.keys(names).reduce( (acc, itemName) => {
+    // if its undefined, its not inside the recipes and we can toss it
+    if (recipes[itemName] === undefined) return acc
+    // gets the items recipe from the itemName
+    const itemRecipe = recipes[itemName]
+    // grabs the ItemId from the itemRecipe
+    const { ItemId } = itemRecipe
+    // finds the item in the mock data
+    const itemData = itemDump.find( item => item.Id === ItemId )
+    const reagentData = getRecipeItemsList(itemRecipe, itemDump)
+    // returns an object to be used
+    return {
+      ...acc,
+      [ItemId]: {
+        ...itemData,
+        recipe: {
+          ...itemRecipe
+        },
+        reagentData
+      }
+    }
+  }, {})
+}
 
 
-
-// used to fetch the item data from our api. but since right now we're just
-// using our local mocdata, it doesn't need to be hooked up
+// used to fetch the item data from our api. but right now we're just
+// using our local mockdata, it doesn't need to be hooked up
 export function fetchItemData() {
   return(dispatch) => {
     dispatch({ type: PARSE_PROFESSION_ITEMS, payload: parseNamesIntoIds() })
-    const parseNeededItems = (itemDump) => {
-      return Object.keys(names).reduce( (acc, itemName) => {
-        // if its undefined, its not inside the recipes and we can toss it
-        if (recipes[itemName] === undefined) return acc
-        // gets the items recipe from the itemName
-        const itemRecipe = recipes[itemName]
-        // grabs the ItemId from the itemRecipe
-        const { ItemId } = itemRecipe
-        // finds the item in the mock data
-        const itemData = itemDump.find( item => item.Id === ItemId )
-        const reagentData = getRecipeItemsList(itemRecipe, itemDump)
-        // returns an object to be used
-        return {
-          ...acc,
-          [ItemId]: {
-            ...itemData,
-            recipe: {
-              ...itemRecipe
-            },
-            reagentData
-          }
-        }
-      }, {})
-    }
     const mockpayload = parseNeededItems(mockData)
     dispatch({ type: FETCH_ITEM_DATA, payload: mockData })
     dispatch({ type: PARSE_ITEM_DATA, payload: mockpayload })
