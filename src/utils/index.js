7383d35e3ece5935d4398e02ebb2d112a0e84039
Blayne Marjama <blayne.marjama@gmail.com>
2018-04-11 00:35:33 -0700

    moved to reduce instead of foreach

--- src/utils/index.js
+++ src/utils/index.js
@@ -13,18 +13,25 @@ export const convertToGold = (price) => {
 // goes through the recipe for the intended item. goes through all their ranks and
 // creates a list of the recipes needed to pass down to the component
 export const getRecipeItemsList = (recipeItem, rawData) => {
-    const recipeList = {};
     // TODO: need to do a check if the item is undefined, then spit out errors.
     // grabs the recipe of the specific item inside the recipe file.
-    recipeItem.RecipeRank.forEach(rankObj => {
-      return rankObj.Reagents.forEach(reagent => {
+    return recipeItem.RecipeRank.reduce((mainAcc, rankObj) => {
+      const rankReagents = rankObj.Reagents.reduce((acc, reagent) => {
+        // don't need to et once it's already been found
+        if ( mainAcc[reagent.Id] !== undefined) return
         if ( reagent.Id === 124124){
           console.log(`Blood of Sargeras being referenced in "${recipeItem.Name}" recipe.`)
-          return recipeList[reagent.Id] = mockBloodofSargeras
+          return {
+            ...acc,
+            [reagent.Id]: mockBloodofSargeras
+          }
         }
         if ( reagent.Id === 151568){
           console.log(`Primal Sargerite being referenced in "${recipeItem.Name}" recipe.`)
-          return recipeList[reagent.Id] = mockBloodofSargeras
+          return {
+            ...acc,
+            [reagent.Id]: mockBloodofSargeras
+          }
         }
         // if the items ID is undefined, we want to let us know.
         if (rawData.find( item => item.Id === reagent.Id) === undefined){
@@ -32,9 +39,14 @@ export const getRecipeItemsList = (recipeItem, rawData) => {
         }
         // maps over the RecipeRank, grabbing all of the Reagents and placing them
         // inside the recipeList
-        recipeList[reagent.Id] = rawData.find( item => item.Id === reagent.Id);
-      });
-    });
-    // returns the newly created recipeList object
-    return recipeList;
+        return {
+          ...acc,
+          [reagent.Id]: rawData.find( item => item.Id === reagent.Id)
+        }
+      }, {});
+      return {
+        ...mainAcc,
+        ...rankReagents
+      }
+    }, {});
   };
